13833ab87d7a85fff7eba5990e92582799962244
tsunoda <tsunoda@aomori-u.ac.jp>
2023-01-18 22:15:05 +0900

    FirestoreにURLと日時登録、StreamBuilderによる表示機能実装、最初からFirebaseにログインする方式に変更

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await signInFirebase();
   runApp(const MyApp());
+}
+
+Future<void> signInFirebase() async {
+  final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: 'test01@gmail.com',
+    password: '8nnDp82xM',
+  );
+  if (FirebaseAuth.instance.currentUser != null) {
+    print('########## ログイン失敗');
+  }
+}
+
+Future<void> signOutFirebase() async {
+  await FirebaseAuth.instance.signOut();
 }
 
 class MyApp extends StatelessWidget {
@@ -31,37 +47,65 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final User user = FirebaseAuth.instance.currentUser!;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('ぽーとふぉりお'),
       ),
-      body: Column(
-        children: [
-          Text('てきすと'),
-          OutlinedButton(
-            onPressed: () async {
-              await signInFirebase();
-              await addPicture();
-              await signOutFirebase();
-            },
-            child: Text('画像追加'),
-          ),
-        ],
-      ),
-    );
-  }
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users/${user.uid}/portfolio')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-  Future<void> signInFirebase() async {
-    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: 'test01@gmail.com',
-      password: '8nnDp82xM',
+            return Column(children: [
+              Text('てきすと'),
+              OutlinedButton(
+                onPressed: () async {
+                  await addPicture();
+                },
+                child: Text('画像追加'),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Column(
+                            children: [
+                              Ink.image(
+                                image: NetworkImage(
+                                  snapshot.data!.docs[index]
+                                      .get('imageURL'),
+                                ),
+                                height: 240,
+                                fit: BoxFit.contain,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(16).copyWith(bottom: 0),
+                                child: Text(
+                                  snapshot.data!.docs[index]
+                                      .get('createdAt')
+                                      .toDate()
+                                      .toLocal()
+                                      .toString(),
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }))
+            ]);
+          }),
     );
-    if (FirebaseAuth.instance.currentUser != null) {
-    }
-  }
-
-  Future<void> signOutFirebase() async {
-    await FirebaseAuth.instance.signOut();
   }
 
   Future<void> addPicture() async {
@@ -72,15 +116,27 @@ class _MyHomePageState extends State<MyHomePage> {
     if (result != null) {
       final User user = FirebaseAuth.instance.currentUser!;
 
+      // Storageに登録
       final int timestamp = DateTime.now().microsecondsSinceEpoch;
       final File file = File(result.files.single.path!);
       final String name = file.path.split('/').last;
       final String path = '${timestamp}_$name';
       final TaskSnapshot task = await FirebaseStorage.instance
           .ref()
-          .child('users/${user.uid}/photos') // フォルダ名
-          .child(path) // ファイル名
-          .putFile(file); // 画像ファイル
+          .child('users/${user.uid}/portfolio')
+          .child(path)
+          .putFile(file);
+
+      // Firestoreに登録
+      final String imageURL = await task.ref.getDownloadURL();
+      final data = {
+        'imageURL': imageURL,
+        'createdAt': Timestamp.now(),
+      };
+      await FirebaseFirestore.instance
+          .collection('users/${user.uid}/portfolio')
+          .doc()
+          .set(data);
     }
   }
 }
